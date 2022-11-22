6baa9c8ebc55befc4a94a236191d71fa0c74a47a
Murat Jummyyev <muratjummyyev@Murats-MacBook-Air.local>
2022-11-21 23:52:49 -0500

    registration service

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,8 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { UserRegistrationDto } from '../dto/user-registration.dto';
 import { User } from '../models';
-import handleDBError from '../utils/handleDBError';
-import { HttpError } from '../utils/httpError';
 
 export const registration = async (
   req: Request,
@@ -10,17 +8,12 @@ export const registration = async (
   next: NextFunction
 ) => {
   const payload: UserRegistrationDto = new UserRegistrationDto(req.body);
-
-  const validation = payload.validate();
-  if (!validation.isValid) {
-    const error = HttpError.badRequest(validation.message);
-    return next(error);
-  }
   try {
     const newUser = await User.create({ ...payload });
     res.json(newUser);
   } catch (error: any) {
-    next(handleDBError(error));
+    error.status = 400;
+    next(error);
   }
 };
 
--- server/src/dto/user-registration.dto.ts
+++ server/src/dto/user-registration.dto.ts
@@ -8,20 +8,4 @@ export class UserRegistrationDto {
     this.password = reqBody.password;
     this.role = reqBody.role;
   }
-
-  validate = () => {
-    const result = {
-      isValid: true,
-      message: 'Pass',
-    };
-    const emailFilter =
-      /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!emailFilter.test(this.email)) {
-      result.isValid = false;
-      result.message = 'Please provide valid email';
-    }
-    return result;
-  };
-
-  // TODO: Create utility method for payload validation
 }
